2425740f9919fb46e5e4d58b192e9aedad9fcf54
sgarver <stiv3n@pm.me>
2022-01-06 17:39:39 -0800

    Add route for static resources

--- myApp.js
+++ myApp.js
@@ -4,6 +4,10 @@ var app = express();
 
 app.get("/demo", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
+
+    console.log(__dirname);
+
+    app.use('/public', express.static("public"));
 });
 
 
