f440de8d7bcf01ec6040b0ac813f2dc9e15809d1
sgarver <stiv3n@pm.me>
2022-01-09 13:03:27 -0800

    Add standard route for static resources

--- myApp.js
+++ myApp.js
@@ -7,6 +7,7 @@ app.get("/", (req, res) => {
 
     console.log(__dirname);
 
+    app.use(express.static(__dirname + "/public"));
     app.use("/public", express.static(__dirname + "/public"));
 });
 
