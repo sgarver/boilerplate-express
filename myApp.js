7bae6b39853e8268087c035a404b9e9f7dc58ba5
sgarver <stiv3n@pm.me>
2022-01-09 13:47:27 -0800

    Update static resource reference

--- myApp.js
+++ myApp.js
@@ -3,12 +3,13 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
 //path.join(__dirname, 'public')
 
 
-app.use("/public", express.static(path.join(__dirname, "/public")));
+//app.use("/public", express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
