1d2936e797f5fb4ffb8df180f5f57cd95b9cd04a
sgarver <stiv3n@pm.me>
2022-01-10 12:44:10 -0800

    Add json route

--- myApp.js
+++ myApp.js
@@ -3,10 +3,15 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "views")));
+//app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "views")));
+
+const data = {"message": "Hello json"}
+
+app.get("/json", (req, res) => {
+    res.json(data);
+});
 
 module.exports = app;
-
 
 
