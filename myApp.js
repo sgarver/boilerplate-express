263b9b7412d871254ae4e06da434e52287d172f8
sgarver <stiv3n@pm.me>
2022-01-10 18:14:04 -0800

    Transform json when MESSAGE_STYLE=uppercase

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 var path = require('path');
-
 var express = require('express');
 var app = express();
+
+require('dotenv').config();
 
 app.use(express.static("public"));
 app.use("/public", express.static("public"));
@@ -9,11 +10,15 @@ app.use("/public", express.static("public"));
 const data = {"message": "Hello json"};
 
 app.get("/json", (req, res) => {
+
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        data.message = data.message.toLocaleUpperCase();
+    }
+
     res.json(data);
 });
 
 app.get("/", (req, res) => {
-console.log(__dirname);
     res.sendFile(__dirname + "/views/index.html");
 });
 
